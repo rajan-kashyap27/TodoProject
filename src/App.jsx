1299afdc9c01d55a35e207ed2a9f39d97836605a
unknown <rajankashyap3057@gmail.com>
2025-06-27 21:12:27 +0530

    Added new component Hello.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Hello from './components/Todo/Hello'
 import Todo  from './components/Todo/Todo'
 import './components/Todo/Todo.css'
 const App = () => {
   return (
     <>
-    <Todo/>
+    {/* <Todo/> */}
+    <Hello/>
     </>
   )
 }
